6defe5067be33d818667000e39b90a2ebe773213
Keith Peters <kp@bit-101.com>
2023-12-23 23:13:56 -0500

    day 01 in the can.

--- days/days.go
+++ days/days.go
@@ -13,3 +13,42 @@ type Day struct {
 	RenderFrame render.FrameFunc
 	Target      int
 }
+
+// GetDay returns the day based on the passed string.
+func GetDay(day string) Day {
+	return daymap[day]
+}
+
+var daymap = map[string]Day{
+	"day01": Day01,
+	"day02": Day02,
+	"day03": Day03,
+	"day04": Day04,
+	"day05": Day05,
+	"day06": Day06,
+	"day07": Day07,
+	"day08": Day08,
+	"day09": Day09,
+	"day10": Day10,
+	"day11": Day11,
+	"day12": Day12,
+	"day13": Day13,
+	"day14": Day14,
+	"day15": Day15,
+	"day16": Day16,
+	"day17": Day17,
+	"day18": Day18,
+	"day19": Day19,
+	"day20": Day20,
+	"day21": Day21,
+	"day22": Day22,
+	"day23": Day23,
+	"day24": Day24,
+	"day25": Day25,
+	"day26": Day26,
+	"day27": Day27,
+	"day28": Day28,
+	"day29": Day29,
+	"day30": Day30,
+	"day31": Day31,
+}
--- days/gen01.go
+++ days/gen01.go
@@ -2,7 +2,12 @@
 package days
 
 import (
+	"log"
+
 	"github.com/bit101/bitlib/blmath"
+	"github.com/bit101/bitlib/easing"
+	"github.com/bit101/bitlib/geom"
+	"github.com/bit101/bitlib/random"
 	cairo "github.com/bit101/blcairo"
 	"github.com/bit101/blcairo/target"
 )
@@ -13,9 +18,79 @@ var Day01 = Day{
 	ImageHeight: 800,
 	VideoWidth:  400,
 	VideoHeight: 400,
-	VideoTime:   2,
+	VideoTime:   5,
 	RenderFrame: Day01Render,
 	Target:      target.Video,
+}
+
+const numPoints = 50000
+
+var (
+	pointsA geom.PointList
+	pointsB geom.PointList
+	pointsC geom.PointList
+	pointsD geom.PointList
+)
+
+func init() {
+	random.Seed(0)
+	width := Day01.VideoWidth
+	height := Day01.VideoHeight
+
+	surfaceA := cairo.NewSurface(width, height)
+	contextA := cairo.NewContext(surfaceA)
+	contextA.SetFontSize(100)
+	contextA.SetSourceRGB(0.2, 0.2, 0.2)
+	contextA.FillText("genuary", 5, 100)
+	dataA, err := contextA.Surface.GetData()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	surfaceB := cairo.NewSurface(width, height)
+	contextB := cairo.NewContext(surfaceB)
+	contextB.SetFontSize(160)
+	contextB.SetSourceRGB(0.2, 0.2, 0.2)
+	contextB.FillText("2024", 14, 350)
+	dataB, err := contextB.Surface.GetData()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	pointsA = geom.NewPointList()
+	pointsB = geom.NewPointList()
+	pointsC = geom.NewPointList()
+	pointsD = geom.NewPointList()
+
+	for i := 0; i < numPoints; i++ {
+		pointsA.Add(geom.RandomPointInRect(50, 50, width-100, height-100))
+	}
+
+	i := 0
+	for i < numPoints {
+		p := geom.RandomPointInRect(0, 0, width-1, height-1)
+		x, y := p.Coords()
+		index := (int(y)*int(width) + int(x)) * 4
+		if dataA[index] > 0 {
+			pointsB.Add(p)
+			i++
+		}
+	}
+
+	for i := 0; i < numPoints; i++ {
+		pointsC.Add(geom.RandomPointInCircle(width/2, height/2, width*0.4, true))
+	}
+
+	i = 0
+	for i < numPoints {
+		p := geom.RandomPointInRect(0, 0, width-1, height-1)
+		x, y := p.Coords()
+		index := (int(y)*int(width) + int(x)) * 4
+		if dataB[index] > 0 {
+			pointsD.Add(p)
+			i++
+		}
+	}
 }
 
 // Day01Render is for genuary 1
@@ -24,12 +99,46 @@ var Day01 = Day{
 //revive:disable-next-line:unused-parameter
 func Day01Render(context *cairo.Context, width, height, percent float64) {
 	context.BlackOnWhite()
-	context.Save()
-	context.TranslateCenter()
-	context.DrawAxes(0.25)
-	r := blmath.LerpSin(percent, 50, width/2)
-	sphere := cairo.NewSphere(0, 0, r, 1, 0, 0)
-	sphere.SetShadowColor(0.2, 0, 0)
-	sphere.Draw(context)
-	context.Restore()
+	ease := easing.QuarticEaseInOut
+	if percent < 0.25 {
+		percent *= 4
+		for i := 0; i < numPoints; i++ {
+			t := ease(percent, 0, 1)
+			pA := pointsA[i]
+			pB := pointsB[i]
+			p := geom.LerpPoint(t, pA, pB)
+			p.Randomize(-5, 5)
+			context.FillPoint(p.X, p.Y, 0.5)
+		}
+	} else if percent < 0.5 {
+		percent = blmath.Map(percent, 0.25, 0.5, 0, 1)
+		for i := 0; i < numPoints; i++ {
+			t := ease(percent, 0, 1)
+			pA := pointsB[i]
+			pB := pointsC[i]
+			p := geom.LerpPoint(t, pA, pB)
+			p.Randomize(-5, 5)
+			context.FillPoint(p.X, p.Y, 0.5)
+		}
+	} else if percent < 0.75 {
+		percent = blmath.Map(percent, 0.5, 0.75, 0, 1)
+		for i := 0; i < numPoints; i++ {
+			t := ease(percent, 0, 1)
+			pA := pointsC[i]
+			pB := pointsD[i]
+			p := geom.LerpPoint(t, pA, pB)
+			p.Randomize(-5, 5)
+			context.FillPoint(p.X, p.Y, 0.5)
+		}
+	} else {
+		percent = blmath.Map(percent, 0.75, 1, 0, 1)
+		for i := 0; i < numPoints; i++ {
+			t := ease(percent, 0, 1)
+			pA := pointsD[i]
+			pB := pointsA[i]
+			p := geom.LerpPoint(t, pA, pB)
+			p.Randomize(-5, 5)
+			context.FillPoint(p.X, p.Y, 0.5)
+		}
+	}
 }
